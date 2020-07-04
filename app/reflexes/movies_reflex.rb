996d46157cfcf62a49ea2d2e5ccf200a43393010
jasonkim7288 <jason.kim7288@gmail.com>
2020-07-04 15:08:40 +1000

    add comment edit function

--- app/reflexes/movies_reflex.rb
+++ app/reflexes/movies_reflex.rb
@@ -11,4 +11,23 @@ class MoviesReflex < ApplicationReflex
       MoviesUser.create(user_id: current_user_id, movie_id: current_movie_id)
     end
   end
+
+  def edit
+    @edit_mode = element.dataset[:comment_id].to_i
+  end
+
+  def submit
+    comment = Comment.find_by(id: element.dataset[:comment_id])
+    comment.update(params.require(:comment).permit(:body))
+    clear_edit_mode
+  end
+
+  def cancel_edit
+    clear_edit_mode
+  end
+
+  private
+    def clear_edit_mode
+      @edit_mode = nil
+    end
 end
